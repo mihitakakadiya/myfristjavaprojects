b75cf55d8d2ca882d028646eb965a5a888f85029
mihitakakadiya <mihitakakadiya1547@gmail.com>
2025-04-16 10:21:03 +0530

    student model done

---
+++ src/main/java/com/example/gitgithubsample_project/GitgithubsampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitgithubsample_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitgithubsampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitgithubsampleProjectApplication.class, args);
+	}
+
+}
